ed8b0015275558bd166e37c31ee2e2046b93c3ea
Benjamin Jaton <benjamin.jaton@gmail.com>
2019-06-06 12:52:20 -0700

    added hrv

--- shouldi.rb
+++ shouldi.rb
@@ -19,44 +19,51 @@ current_car_events = [
     name: "insurance",
     frequency: 30,
     cost: 175,
-    starts_after: 0,
+    starts_after: 15,
     stops_after: 9999999
   },
   {
     name: 'new tires',
     frequency: 2*365,
     cost: 600,
+    starts_after: 1*30,
+    stops_after: 9999999
+  },
+  {
+    name: 'new clutch',
+    frequency: 2.5*365,
+    cost: 4000,
     starts_after: 0,
     stops_after: 9999999
   },
   {
     name: 'major repairs',
     frequency: 2*365,
-    cost: 2500,
-    starts_after: 0,
+    cost: 1500,
+    starts_after: -1,
     stops_after: 9999999
   },
   {
      name: 'minor repairs',
     frequency: 6*30,
     cost: 300,
-    starts_after: 0,
+    starts_after: 6*30,
     stops_after: 9999999
   }
 ];
 
-tesla_car_events = [
+honda_hrv_car_events = [
   {
-    name: "replace battery",
-    frequency: 10*365,
-    cost: 6000,
+    name: "oil changes",
+    frequency: 5 * 30,
+    cost: 45,
     starts_after: 0,
     stops_after: 9999999
   },
   {
-    name: "electricity",
+    name: "gas",
     frequency: 6,
-    cost: 20,
+    cost: 40,
     starts_after: 0,
     stops_after: 9999999
   },
@@ -64,22 +71,22 @@ tesla_car_events = [
     name: "insurance",
     frequency: 30,
     cost: 175,
-    starts_after: 0,
+    starts_after: 15,
+    stops_after: 9999999
+  },
+  {
+    name: 'new tires',
+    frequency: 2*365,
+    cost: 600,
+    starts_after: 1*30,
     stops_after: 9999999
   },
   {
     name: "loan",
     frequency: 30,
-    cost: 500,
-    starts_after: 0,
+    cost: 362,
+    starts_after: -1,
     stops_after: 5*365
-  },
-  {
-    name: 'new tires',
-    frequency: 2*365,
-    cost: 800,
-    starts_after: 0,
-    stops_after: 9999999
   },
   {
     name: 'major repairs',
@@ -91,9 +98,89 @@ tesla_car_events = [
   {
      name: 'minor repairs',
     frequency: 6*30,
-    cost: 100,
+    cost: 150,
+    starts_after: 0,
+    stops_after: 9999999
+  }
+];
+
+tesla_car_events = [
+  {
+    name: "replace battery",
+    frequency: 11*365,
+    cost: 6000,
+    starts_after: -1,
+    stops_after: 9999999
+  },
+  {
+    name: "electricity",
+    frequency: 6,
+    cost: 20,
+    starts_after: 5*30,
+    stops_after: 9999999
+  },
+  {
+    name: "insurance",
+    frequency: 30,
+    cost: 175,
+    starts_after: -1,
+    stops_after: 9999999
+  },
+  {
+    name: "loan",
+    frequency: 30,
+    cost: 800,
+    starts_after: -1,
+    stops_after: 5*365
+  },
+  {
+    name: 'new tires',
+    frequency: 2*365,
+    cost: 800,
+    starts_after: -1,
+    stops_after: 9999999
+  },
+  {
+    name: 'major repairs',
+    frequency: 2*365,
+    cost: 1200,
     starts_after: 5*365,
     stops_after: 9999999
+  },
+  {
+    name: 'minor repairs',
+    frequency: 6*30,
+    cost: 100,
+    starts_after: 4*365,
+    stops_after: 9999999
+  },
+  {
+    name: 'electrical outlet',
+    frequency: 9999999,
+    cost: 400,
+    starts_after: 0,
+    stops_after: 3
+  },
+  {
+    name: 'tax credit',
+    frequency: 10*30,
+    cost: -3750,
+    starts_after: -1,
+    stops_after: 12*30,
+  },
+  {
+    name: 'PGE credit',
+    frequency: 1*30,
+    cost: -800,
+    starts_after: -1,
+    stops_after: 1.5*30,
+  },
+  {
+    name: 'deposit',
+    frequency: 9999999,
+    cost: 2500,
+    starts_after: 0,
+    stops_after: 10,
   }
 ];
 
@@ -109,6 +196,11 @@ def run_simulation(events, max_years)
   current_days = 0
   data = []
 
+  events.each_with_index do |event, index|
+    start = event[:starts_after]
+    counters[index] = start == -1 ? event[:frequency] : start
+  end
+
   for y in 0..(max_years-1)
     for m in 0..11
       for d in 0..(COMMON_YEAR_DAYS_IN_MONTH[m] - 1)
@@ -117,7 +209,7 @@ def run_simulation(events, max_years)
           if current_days > e[:starts_after] && current_days < e[:stops_after]
             counters[index] = remaining_days - 1
             if counters[index] <= 0
-              #puts "Paying for #{e[:name]}" if e[:cost] > 200
+              #puts "Event: #{e[:name]}: $#{e[:cost]}" #if e[:cost] > 500 or e[:cost] < -500
               counters[index] = e[:frequency] - counters[index]
               current_car_cost += e[:cost]
             end
@@ -134,7 +226,7 @@ def run_simulation(events, max_years)
   return data
 end
 
-max_years=21
+max_years=15
 g = Gruff::Line.new
 
 glabels = {}
@@ -143,6 +235,12 @@ for y in 1..(max_years)
 end
 g.labels = glabels
 
+puts "Current Ford Focus"
 g.data 'Current Car', run_simulation(current_car_events, max_years)
+puts ""
+puts "Tesla Model 3"
 g.data 'Tesla Model 3', run_simulation(tesla_car_events, max_years)
+puts ""
+puts "Honda HRV"
+g.data 'Honda HRV', run_simulation(honda_hrv_car_events, max_years)
 g.write('costs.png')
